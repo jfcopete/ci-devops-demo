335b0b467f91412f68e9dfb12028916ac3160837
Juan F Copete <jf.copete@uniandes.edu.co>
2024-11-20 17:31:35 -0500

    TDD Trigger CI

--- hello.py
+++ hello.py
@@ -1,2 +1,6 @@
 def h2():
     return "Hello, World!!"
+def suma(a, b):
+    return a + b
+def resta(a, b):
+    return a - b
--- test_hello.py
+++ test_hello.py
@@ -1,4 +1,9 @@
-from hello import h2
+from hello import h2, resta, suma
 
 def test_h2():
     assert h2() == "Hello, World!!"
+def test_suma():
+    assert suma(2, 3) == 5
+def test_resta():
+    assert resta(3, 2) == 1
+    
